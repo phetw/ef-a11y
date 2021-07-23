2da90682ecce3e0e6617681a68d65246123400a6
phew <wasuwat.se@gmail.com>
2021-07-23 15:09:09 +0700

    add components

---
+++ index.js
@@ -0,0 +1,10 @@
+import '@refinitiv-ui/elements/lib/panel';
+import '@refinitiv-ui/elements/lib/panel/themes/halo/dark';
+
+import '@refinitiv-ui/elements/lib/button';
+import '@refinitiv-ui/elements/lib/button/themes/halo/dark';
+
+import '@refinitiv-ui/elements/lib/combo-box';
+import '@refinitiv-ui/elements/lib/combo-box/themes/halo/dark';
+
+import '@refinitiv-ui/halo-theme/dark/imports/native-elements';
